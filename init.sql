d587ec247819a0ca782dd8f8ea14747ce5f3ca56
Chun Yang <acyang97@gmail.com>
2020-10-23 02:37:42 +0800

    added and changed tables

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 --dbname protest
-DROP TABLE IF EXISTS PriceList, Bids, CareTakerAvailability, RequireSpecialCare, SpecialCare, OwnedPets, Category;
-DROP TABLE IF EXISTS  PCSAdmin, PetOwners, PartTime, FullTime, CareTakers, users;
+DROP TABLE IF EXISTS FullTimePriceList, PartTimePriceList, DefaultPriceList, Bids, CareTakerAvailability, RequireSpecialCare, SpecialCare, OwnedPets, Category;
+DROP TABLE IF EXISTS PreferredTransport, ModeOfTransport, PCSAdmin, PetOwners, PartTime, FullTime, CareTakers, users;
 
 CREATE TABLE users(
     username VARCHAR PRIMARY KEY,
@@ -22,6 +22,16 @@ CREATE TABLE CareTakers (
     rating INTEGER DEFAULT 0
 );
 
+CREATE TABLE ModeOfTransport (
+    transport VARCHAR PRIMARY KEY
+);
+
+CREATE TABLE PreferredTransport (
+    username VARCHAR REFERENCES CareTakers(username) ON DELETE CASCADE,
+    transport VARCHAR REFERENCES ModeOfTransport(transport),
+    PRIMARY KEY (username, transport)
+);
+
 CREATE TABLE FullTime (
     username VARCHAR PRIMARY KEY REFERENCES CareTakers(username) ON DELETE CASCADE
 );
@@ -35,13 +45,23 @@ CREATE TABLE Category (
     pettype VARCHAR PRIMARY KEY
 );
 
+INSERT INTO Category VALUES ('Dog');
+INSERT INTO Category VALUES ('Cat');
+INSERT INTO Category VALUES ('Rabbit');
+INSERT INTO Category VALUES ('Hamster');
+INSERT INTO Category VALUES ('Fish');
+INSERT INTO Category VALUES ('Guinea Pig');
+INSERT INTO Category VALUES ('Mice');
+INSERT INTO Category VALUES ('Terrapin');
+INSERT INTO Category VALUES ('Bird');
+
 CREATE TABLE OwnedPets (
-    owner_name VARCHAR references PetOwners(username) ON DELETE CASCADE,
-    petname VARCHAR NOT NULL UNIQUE,
+    owner VARCHAR references PetOwners(username) ON DELETE CASCADE,
+    pet_name VARCHAR NOT NULL UNIQUE,
     category VARCHAR NOT NULL,
     age INTEGER NOT NULL,
     --gender VARCHAR NOT NULL,
-    Primary Key(owner_name, pet_name)
+    Primary Key(owner, pet_name)
 );
 
 CREATE TABLE SpecialCare(
@@ -49,35 +69,73 @@ CREATE TABLE SpecialCare(
 );
 
 CREATE TABLE RequireSpecialCare(
-    owner_name VARCHAR,
+    owner VARCHAR,
     pet_name VARCHAR,
     care VARCHAR REFERENCES SpecialCare(care),
-    FOREIGN KEY(owner_name, pet_name) REFERENCES OwnedPets(owner_name, pet_name),
-    PRIMARY KEY(owner_name, pet_name, care)
+    FOREIGN KEY(owner, pet_name) REFERENCES OwnedPets(owner, pet_name),
+    PRIMARY KEY(owner, pet_name, care)
 );
 
 CREATE TABLE CaretakerAvailability(
     date TIMESTAMP,
     pet_count INTEGER DEFAULT 0,
     leave BOOLEAN,
-    caretaker_name VARCHAR REFERENCES CareTakers(username),
-    PRIMARY KEY(caretaker_name, date)
+    caretaker VARCHAR REFERENCES CareTakers(username),
+    available BOOLEAN NOT NULL DEFAULT true,
+    PRIMARY KEY(caretaker, date)
 );
 
 CREATE TABLE Bids (
-    owner_name VARCHAR,
+    caretaker VARCHAR,
+    owner VARCHAR,
     pet_name VARCHAR,
-    FOREIGN KEY(owner_name, pet_name) REFERENCES OwnedPets(owner_name, pet_name),
+    FOREIGN KEY(owner, pet_name) REFERENCES OwnedPets(owner, pet_name),
     review VARCHAR,
     rating INTEGER, --to be updated after the bid
     mode_of_transport VARCHAR NOT NULL,
     bid_date_time TIMESTAMP NOT NULL,
     credit_card VARCHAR,
-    successful BOOLEAN
+    completed BOOLEAN DEFAULT FALSE,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (caretaker, start_date) REFERENCES CareTakerAvailability(caretaker, date),
+    FOREIGN KEY (caretaker, end_date) REFERENCES CaretakerAvailability(caretaker, date)
     -- no need start date and end date like what tutor said, so maybe just update the CaretakerAvailability table whenever a bid is made.
     -- other attributes that might need: price (of bid),
 );
 
+CREATE TABLE PartTimePriceList (
+  pettype VARCHAR REFERENCES Category(pettype),
+  caretaker VARCHAR REFERENCES CareTakers(username) ON DELETE CASCADE,
+  price NUMERIC NOT NULL,
+  PRIMARY KEY (pettype, caretaker, price)
+);
+
+CREATE TABLE DefaultPriceList (
+  pettype VARCHAR REFERENCES Category(pettype),
+  price NUMERIC,
+  PRIMARY KEY (pettype, price)
+);
+
+INSERT INTO DefaultPriceList VALUES ('Dog', 100);
+INSERT INTO DefaultPriceList VALUES ('Cat', 80);
+INSERT INTO DefaultPriceList VALUES ('Rabbit', 110);
+INSERT INTO DefaultPriceList VALUES ('Hamster', 70);
+INSERT INTO DefaultPriceList VALUES ('Fish', 50);
+INSERT INTO DefaultPriceList VALUES ('Guinea Pig', 150);
+INSERT INTO DefaultPriceList VALUES ('Mice', 50);
+INSERT INTO DefaultPriceList VALUES ('Terrapin', 80);
+INSERT INTO DefaultPriceList VALUES ('Bird', 80);
+
+
+CREATE TABLE FullTimePriceList(
+  caretaker VARCHAR REFERENCES CareTakers(username) ON DELETE CASCADE,
+  price NUMERIC,
+  pettype VARCHAR,
+  FOREIGN KEY (pettype, price) REFERENCES DefaultPriceList(pettype, price),
+  PRIMARY KEY (pettype, caretaker, price)
+);
+/*
 -- should the pricelist username just reference to the caretakers rather than part time and full time seperately?
 -- we could just check if he is a part time or full time. If full time, we will give option to choose price and then isnert into this TABLE
 -- if full time, we will just insert the default price (from somewhere) based on the pettype.
@@ -87,7 +145,7 @@ CREATE TABLE PriceList (
     price NUMERIC NOT NULL,
     PRIMARY KEY(pettype, username, price)
 );
-
+*/
 --INSERT INTO users (username, email, area, password) VALUES ('a', 'a', 'a', 'a');
 --INSERT INTO PetOwners (username, email, area, password) VALUES ('b', 'b', 'a', 'a');
 
