3ad369c8d75d03b32e025ef39ba3106e7319d256
Chun Yang <acyang97@gmail.com>
2020-10-24 20:19:09 +0800

    debug

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, redirect, render_template, flash, url_for, request
-from flask_login import current_user, login_required, login_user, UserMixin, logout_user
+from flask_login import current_user, login_required, login_user, UserMixin, logout_userfrom __init__ import db, login_manager
 
-from __init__ import db, login_manager
 from forms import *
 from tables import *
 from models import Users
