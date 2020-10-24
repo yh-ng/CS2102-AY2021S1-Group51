67026d33feabf01db7a4a63edab20190e79ce333
Chun Yang <acyang97@gmail.com>
2020-10-24 20:12:34 +0800

    Added table to make displaying tables easier

--- FlaskApp/tables.py
+++ FlaskApp/tables.py
@@ -1,7 +1,14 @@
 from flask_table import Table, Col, LinkCol
 
+## Not sure how to delete properly.
+## Look at the deletepet route too.
 class petList(Table):
     classes = ['table', 'table-bordered', 'table-striped']
-    pet_name = Col('pet_name')
-    category = Col('category')
-    age = Col('age')
+    pet_name = Col('Pet Name')
+    category = Col('Category')
+    age = Col('Age')
+    delete = LinkCol('Delete Pet', 'view.deletepet', url_kwargs=dict(pet_nama='pet_name'))
+
+class UserList(Table):
+    classes = ['table', 'table-bordered', 'table-striped']
+    username = Col('username')
