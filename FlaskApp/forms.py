bfb4f89602f9f02593bb6639a5994439e027be42
Chun Yang <acyang97@gmail.com>
2020-10-26 19:12:54 +0800

    added new fields for form

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -20,6 +20,7 @@ class RegistrationForm(FlaskForm):
     areachoice = [('North', 'North'), ('East', 'East'), ('South', 'South'), ('West', 'West'), ('Central', 'Central')]
     mode_of_transport = [('Pet Owner Deliver', 'Pet Owner Deliver'), ('Care Taker Pick Up', 'Care Taker Pick Up'),
                         ('Transfer through PCS Building', 'Transfer through PCS Building'), ('N.A', 'N.A')]
+    mode_of_payment = [('1', 'Credit Card'), ('2', 'Cash'), ('3', 'N.A')]
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
@@ -31,7 +32,10 @@ class RegistrationForm(FlaskForm):
     area = SelectField('Which area do you stay at?', choices=areachoice)
     select1 = SelectField('What do you want to sign up as?', choices=choice1)
     select2 = SelectField('If you have chosen to sign up as a Care Taker, do you want to work part time or full time?', choices=choice2)
-    mode_of_transport = SelectField('If you have chosen to sign up as a Care Taker, How do you prefer the pet you will take care of to be transported?', choices=mode_of_transport)
+    mode_of_transport = SelectField('If you have chosen to sign up as a Care Taker, How do you prefer the pet you will take care of to be transported? Select N.A if you are signing up as a pet owner',
+                                    choices=mode_of_transport)
+    mode_of_payment = SelectField('If you have chosen to sign up as a Care Taker, select how your prefer mode of payment from pet owners. Select N.A if you are signing up as a pet owner',
+                                    choices=mode_of_payment)
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
@@ -94,14 +98,16 @@ class SearchCareTakerForm(FlaskForm):
     employment = [('1', 'Part Time'), ('2', 'Full Time')]
     ratings = [('0', '0'), ('1', '1'), ('2','2'),('3','3'),('4','4'),('5','5')]
     categories = [('Dog', 'Dog'), ('Cat', 'Cat'), ('Rabbit', 'Rabbit'),
-                ('Hamster', 'Hamster'), ('Fish', 'Fish'), ('Guinea Pig', 'Guinea Pig'),
+                ('Hamster', 'Hamster'), ('Fish', 'Fish'),
                 ('Mice', 'Mice'), ('Terrapin', 'Terrapin'), ('Bird', 'Bird')]
     mode_of_transport = [('Pet Owner Deliver', 'Pet Owner Deliver'), ('Care Taker Pick Up', 'Care Taker Pick Up'),
                         ('Transfer through PCS Building', 'Transfer through PCS Building')]
+    mode_of_payment = [('Credit Card', 'Credit Card'), ('Cash', 'Cash')]
     employment_type = SelectField('Employment', choices=employment)
     category = SelectField('Pet Type', choices=categories)
     rating = SelectField('Rating', choices=ratings)
     preferred_mode_of_transport = SelectField('Preferred Mode of Transport of Pet', choices=mode_of_transport)
+    preferred_mode_of_payment = SelectField('Preferred Mode of Payment to the Care Taker', choices=mode_of_payment)
     startDate = DateField('Start date',validators=[InputRequired()],format='%Y-%m-%d')
     endDate = DateField('End date',validators=[InputRequired()],format='%Y-%m-%d')
     submit = SubmitField('Search Caretaker')
